2cf668cc6c181a4b7fb1641e499ea5d7be8141a3
Dhanveersinh Chauhan <dhanveerc08@gmail.com>
2023-03-17 15:24:50 +0530

    first commit

---
+++ dashboard.php
@@ -0,0 +1,63 @@
+<?php
+include("navbar.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+
+</head>
+<link href="bootstrap.min.css" rel="stylesheet" text="text/css">
+<link rel="stylesheet" text="css/text" href="dash.css">
+<body>
+  <form action="docdet.php" method="POST">
+    <div style="background-color:gainsboro;" align="right" class="card-group">
+      <div align="left">
+      <img src="docback.jpg" style="height: 10in;width:10in; border-radius:0.50in;">
+      </div>
+    <div  align="center" style="height:10in;width:5in; ">
+   
+
+    <br/>
+
+  <div style="background-color: white; width:4in;border-radius:0.20in;" align="center" >
+    <div class="card-body text-center">
+    <button name="button1" align="center">
+            <img class="img" src="2.jpeg" ></button>
+    </div>
+  </div>
+  <br/>
+  <div style="background-color: white; width:4in;border-radius:0.20in;" align="center" >
+    <div class="card-body text-center">
+    <button name="button2">
+    <img class="img" src="2.jpeg" ></button>
+    </div>
+  </div>
+  <br/>
+  <div style="background-color: white; width:4in;border-radius:0.20in;" align="center" >
+    <div class="card-body text-center">
+    <button name="button3">
+    <img class="img" src="2.jpeg" ></button>
+
+    </div>
+
+  </div>
+  <br/>
+  <div style="background-color: white; width:4in;border-radius:0.20in;" align="center" >
+    <div class="card-body text-center">
+    <button name="button4">
+    <img class="img" src="2.jpeg" ></button>
+
+  
+  </div>
+  <br/>
+</div> 
+
+</div>
+</div>
+
+
+</section>
+  </form>
+</body>
+</html>
